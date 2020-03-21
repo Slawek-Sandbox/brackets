28563369773fb1ff0cefed2e0f9d8791ab9a21fa
Slawomir Kucab <Slawomir_Kucab@epam.com>
2020-03-21 09:58:49 +0100

    check function implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  //string with characters to be escaped in a regex pattern
+  const brackets = '()[]{}|';
+
+  // flat string array with escaped brackets pairs
+  //e.g. for [['(', ')'], ['1', '2']] => ['\(\)', '12']
+  const bracketsConfigReduced = bracketsConfig.map(x => x.reduce((a,v) => a + (brackets.indexOf(v) >= 0 ? `\\${v}` : v), ''));
+
+  //regular expression for replacing brackets pairs
+  //e.g. for ['\(\)', '12'] => /(?:\(\)|12)/g
+  const rgx = new RegExp(`(?:${bracketsConfigReduced.join('|')})`, 'g');
+
+  //while match is found replace a brackets pairs with ''
+  //e.g. first loop iteration '(())12' => '()'
+  //    second loop iteration '()' => ''
+  while(rgx.test(str))
+    str = str.replace(rgx, '');
+  
+  //if str.length > 0 means there are some brackets without a pair
+  return str.length ? false : true;
 }
